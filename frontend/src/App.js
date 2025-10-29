cef07c79f1b30afd80b5f2e8b1b18854e9cd7a7e
SumayyaVN9 <suminoushad1012gmail.com>
2025-10-29 22:26:30 +0530

    update css and app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 function App(){
   const[tasks,setTasks] = useState([]);
   const[title,setTitle] =useState("");
+  const[editTitle, setEditTitle] = useState("");
+  const[editIndex,setEditIndex] = useState(null)
 
   const addTask = () => {   //ADD
     if(title.trim() === "") return;
@@ -13,22 +15,39 @@ function App(){
     setTitle("");
   }
   
-    const toggleStatus = (index) => {
-      const update=[...tasks];
-      const current = update[index].status;
-       if(current === "pending") update[index].status= "In progress";
-       else if(current === "In progress") update[index].status = "Done";
-       else update[index].status= "pending"
-    }
+      const handleStatusChange = (index, newStatus) => {
+      const updatedTasks = [...tasks];
+      updatedTasks[index].status = newStatus;
+      setTasks(updatedTasks);
+    };
 
     const deleteTask = (index) => {                     //delete
       const update = tasks.filter((_, i) => i != index);
       setTasks(update);
+    };
+
+    const startEditing = (index) => {
+    setEditIndex(index);
+    setEditTitle(tasks[index].title);
+  };
+
+    const saveEdit = (index) => {
+    const updated = [...tasks];
+    updated[index].title = editTitle;
+    setTasks(updated);
+    setEditIndex(null);
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Enter") {
+      saveEdit(index);
     }
+  };
   return(
     <div className="todo-container">
       <h1>MY TO-DO LIST</h1>
       <div className="input-section">
+        
         <input type="text" placeholder="Enter New Task" value={title} onChange={(e) => setTitle(e.target.value)}/>
         <button onClick={addTask}>ADD TASK</button>
         <table>
@@ -45,11 +64,39 @@ function App(){
             {tasks.map((task, index) => (
               <tr key={task.id}>
                 <td>{task.id}</td>
-                <td>{task.title}</td>
-                <td onClick={() => toggleStatus(index)}>
-                  <span className={`status ${task.status.toLowerCase()}`}>{task.status} </span>
+                  <td>
+                {editIndex === index ? (
+                  <input
+                    type="text"
+                    value={editTitle}
+                    onChange={(e) => setEditTitle(e.target.value)}
+                    onKeyDown={(e) => handleKeyDown(e, index)}
+                    className="edit-input"
+                    autoFocus
+                  />
+                ) : (
+                  task.title
+                )}
+              </td>
+                <td>
+                  <select
+                    className="status-dropdown"
+                    value={task.status}
+                    onChange={(e) => handleStatusChange(index, e.target.value)}
+                  >
+                    <option value="ToDo">ToDo</option>
+                    <option value="Pending">Pending</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Done">Done</option>
+                  </select>
                 </td>
-                <td><button>✏️</button></td>
+                <td>
+                {editIndex === index ? (
+                  <button onClick={() => saveEdit(index)}>💾</button>
+                ) : (
+                  <button onClick={() => startEditing(index)}>✏️</button>
+                )}
+              </td>
                 <td><button onClick={() => deleteTask(index)}>🗑️</button></td>
               </tr>
             ))}
